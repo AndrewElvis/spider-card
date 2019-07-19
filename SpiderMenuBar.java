ee10db9c782aac1d0dede334cb60f0f827f5963e
AndrewElvis <40048272+AndrewElvis@users.noreply.github.com>
2019-07-19 16:35:04 +0800

    菜单条

---
+++ SpiderMenuBar.java
@@ -0,0 +1,149 @@
+
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+
+public class SpiderMenuBar extends JMenuBar{
+    
+	//生成spider框架对象
+    Spider main = null;
+   
+    //生成菜单组
+    JMenu jNewGame = new JMenu("游戏");
+    JMenu jHelp = new JMenu("帮助");
+    
+    //生成菜单项
+    JMenuItem jItemAbout = new JMenuItem("关于");
+    JMenuItem jItemOpen = new JMenuItem("新游戏");
+    JMenuItem jItemPlayAgain = new JMenuItem("发牌/D");
+   
+    //生成单选框
+    JRadioButtonMenuItem jRMItemEasy = new JRadioButtonMenuItem("简单：单一花色");
+    JRadioButtonMenuItem jRMItemNormal = new JRadioButtonMenuItem("中级：双花色");
+    JRadioButtonMenuItem jRMItemHard = new JRadioButtonMenuItem("高级：四花色");;
+    
+    JMenuItem jItemExit = new JMenuItem("退出");
+    JMenuItem jItemValid = new JMenuItem("显示可行操作/M");
+    
+    
+    public SpiderMenuBar(Spider spider){
+        
+        this.main = spider;
+        
+        jNewGame.add(jItemOpen);
+        jNewGame.add(jItemPlayAgain);
+        jNewGame.add(jItemValid);
+        
+        jNewGame.addSeparator();
+        jNewGame.add(jRMItemEasy);
+        jNewGame.add(jRMItemNormal);
+        jNewGame.add(jRMItemHard);
+        
+        jNewGame.addSeparator();
+        
+        jNewGame.add(jItemExit);
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(jRMItemEasy);
+        group.add(jRMItemNormal);
+        group.add(jRMItemHard);
+
+        jHelp.add(jItemAbout);
+
+        this.add(jNewGame);
+        this.add(jHelp);
+
+		//为组件添加事件监听并实现
+        //“新游戏”
+		jItemOpen.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                main.newGame();
+            }
+        });
+
+        //“重新发牌”
+		jItemPlayAgain.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                if(main.getC() < 60){
+                    main.deal();
+                }
+            }
+        });
+
+        //"显示可行操作"
+		jItemValid.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                new Show().start();
+            }
+        });
+        
+        //“退出”
+		jItemExit.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                main.dispose();
+                System.exit(0);
+            }
+        });
+		
+		//“简单级别”默认已选
+		jRMItemEasy.setSelected(true);
+        
+		//“简单级别”
+		jRMItemEasy.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                main.setGrade(Spider.EASY);
+                main.initCards();
+                main.newGame();
+            }
+        });
+        
+        //“中级”
+		jRMItemNormal.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                main.setGrade(Spider.NATURAL);
+                main.initCards();
+                main.newGame();
+            }
+        });
+
+        //“高级”
+		jRMItemHard.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                main.setGrade(Spider.HARD);
+                main.initCards();
+                main.newGame();
+            }
+        });
+		//“重新发牌可否选择”
+		jNewGame.addMenuListener(new javax.swing.event.MenuListener() { 
+            public void menuSelected(javax.swing.event.MenuEvent e) {    
+                if(main.getC() < 60){
+                    jItemPlayAgain.setEnabled(true);
+                }
+                else{
+                    jItemPlayAgain.setEnabled(false);
+                }
+            }
+            public void menuDeselected(javax.swing.event.MenuEvent e) {} //取消选择某个菜单时调用
+            public void menuCanceled(javax.swing.event.MenuEvent e) {} //取消菜单时调用
+        });
+        
+        //“关于”
+		jItemAbout.addActionListener(new java.awt.event.ActionListener() { 
+            public void actionPerformed(java.awt.event.ActionEvent e) {    
+                new AboutDialog();
+            }
+        });
+    }
+
+    
+    //构造线程：显示可以执行的操作
+    class Show extends Thread{
+        public void run(){
+            main.showEnableOperator();
+        }
+    }
+ }
